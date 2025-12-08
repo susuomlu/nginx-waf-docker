6e41e4e6ef03426eb56619b19a2e10cffcc5a716
1posix <246737315+1posix@users.noreply.github.com>
2025-12-08 21:41:21 +0100

    Patch setup.sh color

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ NGINX_CONF="$BASE_DIR/config/nginx.conf"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
+YELLOW='\033[38;5;226m'
 NC='\033[0m'
 
 echo -e "${YELLOW}[INIT] Vérification de la structure WAF...${NC}"
